fa44b4e9d9a2d37f558147ac052886651af2f606
Sergey Zalubovskiy <osmosegw@gmail.com>
2017-10-08 00:39:17 +0300

    refactoring

--- src/components/contact-book/contact-edit-block/tests/contact-edit-block.spec.js
+++ src/components/contact-book/contact-edit-block/tests/contact-edit-block.spec.js
@@ -1,13 +1,10 @@
 // Link.react-test.js
 import React from 'react';
 import { shallow } from 'enzyme';
-import configureStore from 'redux-mock-store';
 
 import ContactEditBlock from '../contact-edit-block';
 
 describe('ContactEditBlock', function() {
-  const mockStore = configureStore();
-
   it('can render', function() {
     const component = shallow(
       <ContactEditBlock
